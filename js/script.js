5718b1222c1f568072d3ae9a7e5cddd60a46cb6b
Quimey Fernandez <quimeyanahifernandez@gmail.com>
2025-06-19 20:56:28 -0300

    changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 // console.log("hola mundo");
 
-let precio = parseInt("300 pesos");
-console.log(precio);
+let numeroA = parseInt(prompt("Ingrese un número: "));
+let numeroB = parseInt(prompt("Ingrese un número: "));
+let resultado = numeroA - numeroB;
+console.log(resultado);
+if (resultado > 2){
+    console.log("El resultado es mayor a 2");
+} else {
+    console.log("El resultado es menor a 2");
+}
+let resulado2 = numeroA * numeroB;
+console.log(resulado2);
+
+let nombre = promt("Ingrese su nombre: ");
+let apellido = promt("Ingrese su apellido: ");
+console.log("Bienvenido," nombre)
