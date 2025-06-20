d0a4b948a2edbce4c456d7faa5f597b67e04a722
Quimey Fernandez <quimeyanahifernandez@gmail.com>
2025-06-19 21:08:24 -0300

    ejercicios c9

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // console.log("hola mundo");
-
+/*
 let numeroA = parseInt(prompt("Ingrese un número: "));
 let numeroB = parseInt(prompt("Ingrese un número: "));
 let resultado = numeroA - numeroB;
@@ -11,7 +11,8 @@ if (resultado > 2){
 }
 let resulado2 = numeroA * numeroB;
 console.log(resulado2);
+*/
 
-let nombre = promt("Ingrese su nombre: ");
-let apellido = promt("Ingrese su apellido: ");
-console.log("Bienvenido," nombre)
+let nombre = promt ("Ingrese su nombre: ");
+let apellido = promt ("Ingrese su apellido: ");
+console.log(`Bienvenido, ${nombre} ${apellido}`);
