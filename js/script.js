cb1ca98846295735eabfc0311b3468187719d237
Quimey Fernandez <quimeyanahifernandez@gmail.com>
2025-06-20 22:45:51 -0300

    clase10

--- js/script.js
+++ js/script.js
@@ -13,6 +13,52 @@ let resulado2 = numeroA * numeroB;
 console.log(resulado2);
 */
 
-let nombre = promt ("Ingrese su nombre: ");
-let apellido = promt ("Ingrese su apellido: ");
-console.log(`Bienvenido, ${nombre} ${apellido}`);
+/*
+// Solicitar datos al usuario
+let nombre = prompt("Por favor, ingresa tu nombre:");
+let edadInput = prompt("Hola " + nombre + ", ingresa tu edad:");
+
+let edad = parseInt(edadInput);
+
+//Validar si es un número
+if (isNaN(edad)) {
+    console.log("Error: '" + edadInput + "' no es un número de edad válido.");
+} else if(edad >= 18){
+    console.log("Eres mayor de edad.");
+} else if (edad < 18 && edad > 14){
+    console.log("Eres adolescente");
+} else {
+    console.log("Eres un infante.")
+}
+*/
+
+/*
+// Pedir edad, si esta en lista VIP
+let edad = parseInt(prompt("Ingrese su edad: "))
+let miembroVIP = confirm("¿Sos miembro VIP?: ")
+if (edad >= 18 && miembroVIP) {
+    console.log("Acceso concedido al área VIP.")
+} else if (edad >= 18 && !miembroVIP) {
+    console.log("Acceso concedido al sector general")
+} else {
+    console.log("Acceso denegado.")
+}
+*/
+
+// Lista de productos
+let productos = [
+ { nombre: 'Laptop', descuento: true, precio: 1200},
+ { nombre: 'Celular', descuento: false, precio: 1700 },
+ { nombre: 'Tablet', descuento: true, precio: 3000 },
+ { nombre: 'Auriculares', descuento: true, precio: 4000}
+];
+
+ for (let i = 0; i < productos.length; i++) {
+ let producto = productos[i];
+ if (producto.descuento === true){
+    console.log(`${producto.nombre}: $${producto.precio} (en descuento)`);
+ }
+ if (!producto.descuento){
+    console.log(`${producto.nombre} no está en promoción.`)
+ }
+}
